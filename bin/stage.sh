9ca63f7ad5dd8fea7288e0d6b98aec875dd66b2e
Eli McMakin <elimc184@outlook.com>
2016-02-29 17:30:15 -0600

    Updated the stage.sh script to what is hopefully the correct git repo.

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh 
-git remote add stage git@git.wpengine.com:staging/crometrics.git
+git remote add staging git@git.wpengine.com:staging/crometrics.git
 git push -f stage HEAD:master
