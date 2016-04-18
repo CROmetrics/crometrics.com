1e59bc8a61cc34a0b82fe2c1315a8f508373b8a9
Brian Hall <briandavidhall@gmail.com>
2016-04-18 12:31:22 -1000

    add MailHandler files

---
+++ bin/MailHandler.php
@@ -0,0 +1,30 @@
+<?php
+	$owner_email = 'chris@crometrics.com';
+
+    // Sanitize input.
+    function trim_value(&$value) { $value = trim(strip_tags($value)); }
+    array_filter($_POST, 'trim_value');
+
+	$headers = 'From:' . $_POST["email"] . "\r\n";
+    $headers .= "Reply-To: " . $_POST["email"] . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$subject = 'CROmetrics site lead from ' . $_POST["name"];
+	$messageBody = "";
+
+    $messageBody .= '<p>Visitor: ' . $_POST["name"] . '</p>' . "\n";
+    $messageBody .= '<p>Email Address: ' . $_POST['email'] . '</p>' . "\n";
+    $messageBody .= '<p>Company: ' . $_POST['company'] . '</p>' . "\n";
+    $messageBody .= '<p>Website: ' . $_POST['website'] . '</p>' . "\n";
+    $messageBody .= '<p>Goals: ' .$_POST['message'] . '</p>' . "\n";
+	
+	try{
+		if(!mail($owner_email, $subject, $messageBody, $headers)){
+			throw new Exception('mail failed');
+		}else{
+			echo 'mail sent';
+		}
+	}catch(Exception $e){
+		echo $e->getMessage() ."\n";
+	}
+?>
