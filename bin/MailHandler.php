085eb5a1fadaa7c135a9b647ed5a58d747cba07b
Brian Hall <briandavidhall@gmail.com>
2016-06-08 17:02:53 -1000

    email engineering and leave chris/sales alone for job application submissions

--- bin/MailHandler.php
+++ bin/MailHandler.php
@@ -1,7 +1,12 @@
 <?php
+	// Email addresses
 	$owner_email = 'chris@crometrics.com';
 	$sales_discussion_slack_email = 'v2p6g1n7t7z0v6h9@crometrics.slack.com';
+	$engineering_email = 'p9j5f7t5w5u2c5b2@crometrics.slack.com';
 	$brian_crobot_slack_email = 'l8s3w7k1i4j5t2h8@crometrics.slack.com';
+	// Booleans for whom we should email
+	$mail_engineering = FALSE;
+	$mail_sales = TRUE;
 
     // Sanitize input.
     function trim_value(&$value) { $value = trim(strip_tags($value)); }
@@ -20,32 +25,45 @@
     $messageBody .= '<p>Website: ' . $_POST['website'] . '</p>' . "\n";
     $messageBody .= '<p>Goals: ' .$_POST['message'] . '</p>' . "\n";
 
-    if (strpos($_POST['company'], 'CROmetrics') !== FALSE) {
-	    $messageBody .= 'FOOO';
+    // Email Engineering instead of Sales if somebody's working on the job application
+    if (strpos(strtolower($_POST['company']), 'crometrics') !== FALSE) {
+	    $mail_engineering = TRUE;
+	    $mail_sales = FALSE;
+    }
+
+    // Only email Brian's DM with Crobot if it's a test submission
+    if (strpos(strtolower($_POST['name']), 'mctesterson') !== FALSE) {
+	    $mail_sales = FALSE;
     }
 	
 	try{
-		// Mail Chris
-		/*
-		if(!mail($owner_email, $subject, $messageBody, $headers)){
-			throw new Exception('mail failed');
-		}else{
-			echo 'mail sent';
-		}
-		 */
-		// Mail Sales Discussion
-		/*
-		if(!mail($sales_discussion_slack_email, $subject, $messageBody, $headers)){
-			throw new Exception('mail failed');
-		}else{
-			echo 'mail sent';
-		}
-		 */
-		// Mail my private chat with Crobot
+		// Mail Brian's private chat with Crobot (always)
 		if(!mail($brian_crobot_slack_email, $subject, $messageBody, $headers)){
 			throw new Exception('mail failed');
 		}else{
 			echo 'mail sent';
+		}
+		// Mail Chris and sales-discussion if appropriate
+		if ($mail_sales) {
+			if(!mail($owner_email, $subject, $messageBody, $headers)){
+				throw new Exception('mail failed');
+			}else{
+				echo 'mail sent';
+			}
+			// Mail Sales Discussion
+			if(!mail($sales_discussion_slack_email, $subject, $messageBody, $headers)){
+				throw new Exception('mail failed');
+			}else{
+				echo 'mail sent';
+			}
+		}
+		// Mail Engineering if it's a job application
+		if ($mail_engineering) {
+			if(!mail($engineering_email, $subject, $messageBody, $headers)){
+				throw new Exception('mail failed');
+			}else{
+				echo 'mail sent';
+			}
 		}
 	}catch(Exception $e){
 		echo $e->getMessage() ."\n";
