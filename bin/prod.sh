32d29642b757cc3a3746845e3abd466bdc3100dd
Tom Fuertes <tomfuertes@gmail.com>
2016-07-14 15:34:21 -0500

    header printing

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,9 +1,32 @@
 git remote add production git@git.wpengine.com:production/crometrics.git
+
+echo ""
+echo "*****************************"
+echo "**** FETCHING PRODUCTION ****"
+echo "*****************************"
+echo ""
+
 git fetch production
+
+echo ""
+echo "*****************************"
+echo "********* RAW DIFF **********"
+echo "*****************************"
+echo ""
 
 git diff production/master HEAD
 
+echo ""
+echo "*****************************"
+echo "******* MINIMAL DIFF ********"
+echo "*****************************"
+echo ""
+
 git diff --stat production/master HEAD
+
+echo ""
+echo "***********************"
+echo ""
 
 read -r -p "Deploy HEAD diff above to production? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
