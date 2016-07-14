35f5ba41572e8d9d66c78dc7e762abd270da4863
Tom Fuertes <tomfuertes@gmail.com>
2016-07-14 15:27:06 -0500

    add confirm to prod deploy w/ diff

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,2 +1,14 @@
 git remote add production git@git.wpengine.com:production/crometrics.git
-git push -f production HEAD:master
+git fetch production
+
+git diff production/master HEAD
+
+git diff --stat production/master HEAD
+
+read -r -p "Deploy HEAD diff above to production? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+  git push -f production HEAD:master
+else
+  echo "Please run again and type yes"
+fi
