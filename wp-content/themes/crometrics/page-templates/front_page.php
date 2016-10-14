7a3af4a35d5e74d7fda84f1306f30a358a7f2d20
Tom Fuertes <tomfuertes@gmail.com>
2016-10-14 16:05:12 -0500

    save w/ new line endings (no change)

--- wp-content/themes/crometrics/page-templates/front_page.php
+++ wp-content/themes/crometrics/page-templates/front_page.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template Name: The Front Page
- * 
+ *
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
@@ -12,13 +12,13 @@ get_header(); ?>
 
 <?php
 while(have_posts()): the_post();
-?> 
+?>
 
 <!-- Contact -->
 <div class="modal fade" id="contact-modal" tabindex="-1" role="dialog" aria-labelledby="contact-modal-label">
 <div class="modal-dialog modal-md" role="document">
-  <div class="modal-content">	
-	<?php dynamic_sidebar('Contact Form'); ?>
+  <div class="modal-content">
+  <?php dynamic_sidebar('Contact Form'); ?>
   </div>
 </div>
 </div>
@@ -36,16 +36,16 @@ while(have_posts()): the_post();
 <!-- Example row of columns -->
 <div class="row">
 
-<?php if( get_field('what_we_do_strip') ): ?>							 
-		<?php $i=1; while( has_sub_field('what_we_do_strip') ): ?>
+<?php if( get_field('what_we_do_strip') ): ?>
+    <?php $i=1; while( has_sub_field('what_we_do_strip') ): ?>
   <div class="col-md-4">
-	<img class="center-block" src="<?php the_sub_field('image'); ?>" />
-	<h2 class="text-center"><?php the_sub_field('title'); ?></h2>
-	<p><?php the_sub_field('content'); ?></p>
-  </div>  
-  		<?php $i++; endwhile; ?>		                
-<?php endif; ?>  
-  
+  <img class="center-block" src="<?php the_sub_field('image'); ?>" />
+  <h2 class="text-center"><?php the_sub_field('title'); ?></h2>
+  <p><?php the_sub_field('content'); ?></p>
+  </div>
+      <?php $i++; endwhile; ?>
+<?php endif; ?>
+
 </div>
 </div>
 
@@ -57,18 +57,18 @@ while(have_posts()): the_post();
 <div class="container">
   <h3 class="text-uppercase text-center"><?php the_field( "services_title" ); ?></h3>
   <div class="row">
-  
-<?php if( get_field('services_strip') ): ?>							 
-		<?php $i=1; while( has_sub_field('services_strip') ): ?>	
-	<div class="col-md-4">
-	  <h2 class="text-center"><?php the_sub_field('title'); ?></h2>
-	  <hr class="small brown">
-	  <p><?php the_sub_field('content'); ?></p>
-	</div>
-		<?php $i++; endwhile; ?>		                
-<?php endif; ?>  
 
-	
+<?php if( get_field('services_strip') ): ?>
+    <?php $i=1; while( has_sub_field('services_strip') ): ?>
+  <div class="col-md-4">
+    <h2 class="text-center"><?php the_sub_field('title'); ?></h2>
+    <hr class="small brown">
+    <p><?php the_sub_field('content'); ?></p>
+  </div>
+    <?php $i++; endwhile; ?>
+<?php endif; ?>
+
+
   </div>
 </div>
 </div>
@@ -77,19 +77,19 @@ while(have_posts()): the_post();
 <h3 class="text-uppercase text-center"><?php the_field( "success_title" ); ?></h3>
 <div class="row">
 
-<?php if( get_field('success_strip') ): ?>							 
-		<?php $i=1; while( has_sub_field('success_strip') ): ?>	
+<?php if( get_field('success_strip') ): ?>
+    <?php $i=1; while( has_sub_field('success_strip') ): ?>
   <div class="col-md-6">
-	<img src="<?php the_sub_field('image'); ?>" class="center-block">
-	<div class="divider">
-	  <hr class="small gray">&#8220;
-	  <hr class="small gray">
-	</div>
-	<?php the_sub_field('content'); ?>
+  <img src="<?php the_sub_field('image'); ?>" class="center-block">
+  <div class="divider">
+    <hr class="small gray">&#8220;
+    <hr class="small gray">
   </div>
-  		<?php $i++; endwhile; ?>		                
-<?php endif; ?>    
-  
+  <?php the_sub_field('content'); ?>
+  </div>
+      <?php $i++; endwhile; ?>
+<?php endif; ?>
+
 </div>
 </div>
 
@@ -97,16 +97,16 @@ while(have_posts()): the_post();
 <div class="container">
   <h3 class="text-uppercase text-center"><?php the_field( "who_calls_title" ); ?></h3>
   <div class="row">
-  
-<?php if( get_field('who_calls_strip') ): ?>							 
-		<?php $i=1; while( has_sub_field('who_calls_strip') ): ?>	
-	<div class="col-md-4">
-	  <h2 class="text-center"><?php the_sub_field('title'); ?></h2>
-	  <hr class="small brown">
-	  <?php the_sub_field('content'); ?>
-	</div>
-		<?php $i++; endwhile; ?>		                
-<?php endif; ?>  
+
+<?php if( get_field('who_calls_strip') ): ?>
+    <?php $i=1; while( has_sub_field('who_calls_strip') ): ?>
+  <div class="col-md-4">
+    <h2 class="text-center"><?php the_sub_field('title'); ?></h2>
+    <hr class="small brown">
+    <?php the_sub_field('content'); ?>
+  </div>
+    <?php $i++; endwhile; ?>
+<?php endif; ?>
 
   </div>
 </div>
@@ -121,19 +121,19 @@ while(have_posts()): the_post();
   <h3 class="text-uppercase text-center"><?php echo $awards_titl ; ?></h3>
   <h2 class="text-center"><?php echo $awards_subtitl ; ?></h2>
   <div class="row">
-<?php 
-	if (function_exists('ot_get_option')) {
-	$socail_conten = ot_get_option('awards_icon');?> 
-	
-	<?php foreach($socail_conten as $socail_cont){
-		if($socail_cont['link'] != '' || $socail_cont['image'] != '' ){ ?>
-	<div class="col-md-4">
-	  <a href="<?php echo $socail_cont['link'];?>"><img src="<?php echo $socail_cont['image'];?>" alt="<?php echo $socail_cont['title'];?>" class="center-block"></a>
-	  <h4 class="text-center"><?php echo $socail_cont['title'];?></h4>
-	</div>
-	    <?php } 
-	} ?>	
-<?php } ?> 	
+<?php
+  if (function_exists('ot_get_option')) {
+  $socail_conten = ot_get_option('awards_icon');?>
+
+  <?php foreach($socail_conten as $socail_cont){
+    if($socail_cont['link'] != '' || $socail_cont['image'] != '' ){ ?>
+  <div class="col-md-4">
+    <a href="<?php echo $socail_cont['link'];?>"><img src="<?php echo $socail_cont['image'];?>" alt="<?php echo $socail_cont['title'];?>" class="center-block"></a>
+    <h4 class="text-center"><?php echo $socail_cont['title'];?></h4>
+  </div>
+      <?php }
+  } ?>
+<?php } ?>
   </div>
   <h3 class="text-center"><?php echo $awards_footer_tex ; ?></h3>
 </div>
@@ -143,7 +143,7 @@ while(have_posts()): the_post();
 
 <?php
 endwhile;
-?> 
+?>
 
 
 <?php
