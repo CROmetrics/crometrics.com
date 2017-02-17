b9894c21fa02dbd316015ab7093d157dc6f2f86e
Christian Rios <crios121@gmail.com>
2017-02-17 07:14:11 -0500

    CROmetrics team page template (dev test)

---
+++ wp-content/themes/crometrics/page-crometrics-team.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: CROmetrics - Team Page
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+<!-- Contact -->
+  <div class="modal fade" id="contact-modal" tabindex="-1" role="dialog" aria-labelledby="contact-modal-label">
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+         <?php dynamic_sidebar('Contact Form'); ?>
+      </div>
+    </div>
+  </div>
+  <!-- /Contact -->
+  
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="container-fluid bg-gray-darker">
+    <div class="container">
+      <h3 class="text-uppercase text-light-gray"><?php the_title(); ?></h3>
+      <h2><?php the_field( "subtitle" ); ?></h2>
+    </div>
+  </div>
+  
+  <div class="container">
+    <?php the_content(); ?>
+  </div>
+  
+  
+<?php endwhile; ?>
+
+<?php
+
+get_footer();
