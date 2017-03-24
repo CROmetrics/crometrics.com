9c2db0a2e5de5c39698cf5a05f96f336e9434820
Tom Fuertes <tomfuertes@gmail.com>
2017-03-24 16:28:18 -0500

    checkin readme and staging

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,9 +1,9 @@
 git remote add staging git@git.wpengine.com:staging/crometrics.git
 
-echo "***************************************"
+echo ""
 echo "***************************************"
 echo "https://crometrics.staging.wpengine.com/"
 echo "***************************************"
-echo "***************************************"
+echo ""
 
 git push -f staging HEAD:master
